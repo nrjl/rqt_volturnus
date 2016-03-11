33e0826a077bc29a3a6683c582010d7568b7fa3b
Nicholas Lawrance <lawrancn@onid.oregonstate.edu>
2016-03-11 10:35:17 -0800

    Final commit before moving package to separate repo.

--- src/lcm_gui.cpp
+++ src/lcm_gui.cpp
@@ -1,6 +1,10 @@
 #include "lcm_gui.h"
 #include "ui_lcm_gui.h"
+#include <iostream>
 #include <QThread>
+#include <QLCDNumber>
+#include <iterator>
+#include <sys/time.h>
 //#include <QDateTime>
 //#include <QTime>
 
@@ -10,7 +14,11 @@ namespace rqt_volturnus {
 
 lcm_gui::lcm_gui()
     : rqt_gui_cpp::Plugin(),
-      widget_(0)
+      widget_(0),
+      nav_delay_(0),
+      dvl_delay_(0),
+      pressure_delay_(0),
+      compass_delay_(0)
 {
     setObjectName("LCMGUI");
 }
@@ -29,12 +37,24 @@ void lcm_gui::initPlugin(qt_gui_cpp::PluginContext& context)
     // add widget to the user interface
     context.addWidget(widget_);
 
+    // Make LCDs flat
+    QList<QLCDNumber *> allLCDs = widget_->findChildren<QLCDNumber *>();
+    foreach(QLCDNumber * item, allLCDs) {
+        item->setSegmentStyle(QLCDNumber::Flat);
+    }
+
     // Subscribers
-    nav_sub_ = getNodeHandle().subscribe<lcm_to_ros::STAT_Nav_soln>("/lcm_to_ros/STAT_Nav_soln", 10, &lcm_gui::navMessageCallback, this);
+    nav_sub_ = getNodeHandle().subscribe<lcm_to_ros::nav_solution>("/lcm_to_ros/OPENINS_NAV_SOLUTION", 10, &lcm_gui::navMessageCallback, this);
+    dvl_sub_ = getNodeHandle().subscribe<lcm_to_ros::dvl_stat>("/lcm_to_ros/OPENINS_DVL_STAT", 10, &lcm_gui::dvlMessageCallback, this);
+    compass_sub_ = getNodeHandle().subscribe<lcm_to_ros::compass_stat>("/lcm_to_ros/OPENINS_COMPASS_STAT", 10, &lcm_gui::compassMessageCallback, this);
+    pressure_sub_ = getNodeHandle().subscribe<lcm_to_ros::pressure_stat>("/lcm_to_ros/OPENINS_PRESSURE_STAT", 10, &lcm_gui::pressureMessageCallback, this);
 
     // CONNECTIONS
     // response message receivers
     connect(this, SIGNAL(navMessageReceived()), this, SLOT( on_nav_message_received()) );
+    connect(this, SIGNAL(dvlMessageReceived()), this, SLOT( on_dvl_message_received()) );
+    connect(this, SIGNAL(pressureMessageReceived()), this, SLOT( on_pressure_message_received()) );
+    connect(this, SIGNAL(compassMessageReceived()), this, SLOT( on_compass_message_received()) );
 }
 
 void lcm_gui::shutdownPlugin()
@@ -54,10 +74,27 @@ void lcm_gui::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, const
   // v = instance_settings.value(k)
 }
 
-void lcm_gui::navMessageCallback(const lcm_to_ros::STAT_Nav_soln::ConstPtr& msg)
+void lcm_gui::navMessageCallback(const lcm_to_ros::nav_solution::ConstPtr& msg)
 {
     nav_msg_ = *msg;
     emit navMessageReceived();
+}
+
+void lcm_gui::dvlMessageCallback(const lcm_to_ros::dvl_stat::ConstPtr& msg)
+{
+    dvl_msg_ = *msg;
+    emit dvlMessageReceived();
+}
+void lcm_gui::pressureMessageCallback(const lcm_to_ros::pressure_stat::ConstPtr& msg)
+{
+    pressure_msg_ = *msg;
+    emit pressureMessageReceived();
+}
+
+void lcm_gui::compassMessageCallback(const lcm_to_ros::compass_stat::ConstPtr& msg)
+{
+    compass_msg_ = *msg;
+    emit compassMessageReceived();
 }
 
 void lcm_gui::set_OK(double status, QLabel* qlbl, const QString& text)
@@ -81,28 +118,59 @@ void lcm_gui::on_nav_message_received()
     ui_.heading_LCD->display(nav_msg_.heading);
     ui_.heading_rate_LCD->display(nav_msg_.heading_rate);
 
-    ui_.latitude_LCD->display(nav_msg_.latitude);
-    ui_.longitude_LCD->display(nav_msg_.longitude);
+    ui_.latitude_LCD->display(nav_msg_.last_lonlat_fix_1);
+    ui_.longitude_LCD->display(nav_msg_.last_lonlat_fix_0);
 
     ui_.depth_LCD->display(nav_msg_.depth);
-    ui_.depth_rate_LCD->display(nav_msg_.depth_velocity);
+    ui_.depth_rate_LCD->display(nav_msg_.relative_velocity_z);
 
-    ui_.rel_east_LCD->display(nav_msg_.relative_easting);
-    ui_.rel_east_rate_LCD->display(nav_msg_.relative_easting_velocity);
-    ui_.rel_north_LCD->display(nav_msg_.relative_northing);
-    ui_.rel_north_rate_LCD->display(nav_msg_.relative_northing_velocity);
+    ui_.rel_east_LCD->display(nav_msg_.relative_position_x);
+    ui_.rel_east_rate_LCD->display(nav_msg_.relative_velocity_x);
+    ui_.rel_north_LCD->display(nav_msg_.relative_position_y);
+    ui_.rel_north_rate_LCD->display(nav_msg_.relative_position_y);
 
     ui_.speed_over_ground_LCD->display(nav_msg_.speed_over_ground);
     ui_.course_over_ground_LCD->display(nav_msg_.course_over_ground);
 
-    set_OK(nav_msg_.attitude_OK, ui_.attitude_OK, "Attitude");
-    set_OK(nav_msg_.altitude_OK, ui_.altitude_OK, "Altitude");
-    set_OK(nav_msg_.depth_OK, ui_.depth_OK, "Depth");
-    set_OK(nav_msg_.GPS_OK, ui_.GPS_OK, "GPS");
-    set_OK(nav_msg_.relative_position_OK, ui_.rel_pos_OK, "Relative Position");
-    set_OK(nav_msg_.absolute_position_OK, ui_.abs_pos_OK, "Absolute Position");
-    set_OK(nav_msg_.bottom_lock_OK, ui_.DVL_OK, "DVL Bottom Lock");
+    set_OK(nav_msg_.attitude_ok, ui_.attitude_OK, "Attitude");
+    set_OK(nav_msg_.altitude_ok, ui_.altitude_OK, "Altitude");
+    set_OK(nav_msg_.depth_ok, ui_.depth_OK, "Depth");
+    set_OK(nav_msg_.absolute_position_ok, ui_.GPS_OK, "GPS");
+    set_OK(nav_msg_.relative_position_ok, ui_.rel_pos_OK, "Relative Position");
+    set_OK(nav_msg_.absolute_position_ok, ui_.abs_pos_OK, "Absolute Position");
+    set_OK(nav_msg_.have_bottom_lock, ui_.DVL_OK, "DVL Bottom Lock");
 }
+
+void lcm_gui::on_dvl_message_received()
+{
+    set_OK(dvl_msg_.btm_channel.lock, ui_.dvl_btm_lock, "LOCK");
+    ui_.dvl_btm_velx_LCD->display(dvl_msg_.btm_channel.vel_x);
+    ui_.dvl_btm_vely_LCD->display(dvl_msg_.btm_channel.vel_y);
+    ui_.dvl_btm_velz_LCD->display(dvl_msg_.btm_channel.vel_z);
+    ui_.dvl_btm_alt_LCD->display(dvl_msg_.btm_channel.altitude);
+
+    set_OK(dvl_msg_.ref_channel.lock, ui_.dvl_ref_lock, "LOCK");
+    ui_.dvl_ref_velx_LCD->display(dvl_msg_.ref_channel.vel_x);
+    ui_.dvl_ref_vely_LCD->display(dvl_msg_.ref_channel.vel_y);
+    ui_.dvl_ref_velz_LCD->display(dvl_msg_.ref_channel.vel_z);
+    ui_.dvl_ref_alt_LCD->display(dvl_msg_.ref_channel.altitude);
+}
+
+void lcm_gui::on_pressure_message_received()
+{
+    ui_.temperature_LCD->display(pressure_msg_.channels[0].value);
+    ui_.pressure_LCD->display(pressure_msg_.pressure_psi);
+}
+
+void lcm_gui::on_compass_message_received()
+{
+    set_OK(compass_msg_.attitude_valid, ui_.compass_att_status, "Attitude valid");
+    ui_.compass_roll_LCD->display(compass_msg_.attitude_roll_deg);
+    ui_.compass_pitch_LCD->display(compass_msg_.attitude_pitch_deg);
+    ui_.compass_heading_LCD->display(compass_msg_.attitude_heading_deg);
+    ui_.compass_distortion_LCD->display(compass_msg_.channels[0].value);
+}
+
 
 } //namespace
 
--- src/lcm_gui.h
+++ src/lcm_gui.h
@@ -6,7 +6,10 @@
 #include <QWidget>
 #include <QString>
 #include <ros/ros.h>
-#include "lcm_to_ros/STAT_Nav_soln.h"
+#include "lcm_to_ros/nav_solution.h"
+#include "lcm_to_ros/compass_stat.h"
+#include "lcm_to_ros/dvl_stat.h"
+#include "lcm_to_ros/pressure_stat.h"
 //#include <ros/console.h>
 
 
@@ -27,20 +30,79 @@ public:
 
 private slots:
     void on_nav_message_received();
+    void on_dvl_message_received();
+    void on_pressure_message_received();
+    void on_compass_message_received();
 
 signals:
     void navMessageReceived();
+    void dvlMessageReceived();
+    void pressureMessageReceived();
+    void compassMessageReceived();
 
 private:
     Ui::VolturnusWidget ui_;
     QWidget* widget_;
-    lcm_to_ros::STAT_Nav_soln nav_msg_;
-    void navMessageCallback(const lcm_to_ros::STAT_Nav_soln::ConstPtr& msg);
+    lcm_to_ros::nav_solution nav_msg_;
+    lcm_to_ros::dvl_stat dvl_msg_;
+    lcm_to_ros::pressure_stat pressure_msg_;
+    lcm_to_ros::compass_stat compass_msg_;
+    void navMessageCallback(const lcm_to_ros::nav_solution::ConstPtr& msg);
+    void dvlMessageCallback(const lcm_to_ros::dvl_stat::ConstPtr& msg);
+    void pressureMessageCallback(const lcm_to_ros::pressure_stat::ConstPtr& msg);
+    void compassMessageCallback(const lcm_to_ros::compass_stat::ConstPtr& msg);
     void set_OK(double status, QLabel *qlbl, const QString& text);
 
 protected:
     ros::Subscriber nav_sub_;
+    ros::Subscriber dvl_sub_;
+    ros::Subscriber pressure_sub_;
+    ros::Subscriber compass_sub_;
+    double nav_delay_;
+    double dvl_delay_;
+    double pressure_delay_;
+    double compass_delay_;
 };
+
+//class LightWidget : public QWidget
+//{
+//    Q_OBJECT
+//    Q_PROPERTY(bool on READ isOn WRITE setOn)
+
+//public:
+//    LightWidget(const QColor &coloron, const QColor &coloroff, QWidget *parent = 0)
+//        : QWidget(parent), m_coloron(coloron), m_coloroff(coloroff), m_on(false) {}
+
+//    bool isOn() const
+//        { return m_on; }
+//    void setOn(bool on)
+//    {
+//        if (on == m_on)
+//            return;
+//        m_on = on;
+//        update();
+//    }
+
+//public slots:
+//    void turnOff() { setOn(false); }
+//    void turnOn() { setOn(true); }
+
+//protected:
+//    virtual void paintEvent(QPaintEvent *)
+//    {
+//        QPainter painter(this);
+//        painter.setRenderHint(QPainter::Antialiasing);
+//        if (m_on)
+//            painter.setBrush(m_coloron);
+//        else
+//            painter.setBrush(m_coloroff);
+//        painter.drawEllipse(0, 0, width(), height());
+//    }
+
+//private:
+//    QColor m_coloron, m_coloroff;
+//    bool m_on;
+//};
 
 
 } // namespace
